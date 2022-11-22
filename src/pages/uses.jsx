38a52207bbe6f639250d23ad31645ddcf6925f4d
JC <60675751+jorge-castello@users.noreply.github.com>
2022-11-22 00:24:00 -0800

    Create initial website (#1)
    * working on personal site
    
    * working with a decent website-shell at this point

---
+++ src/pages/uses.jsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export default function Uses() {
+  return (
+    <>
+      <Head>
+        <title>Uses - Jorge Castello</title>
+        <meta
+          name="description"
+          content="Software I use, gadgets I love, and other things I recommend."
+        />
+      </Head>
+      <SimpleLayout
+        title="Software I use, gadgets I love, and other things I recommend."
+        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      >
+        <div className="space-y-20">
+          <ToolsSection title="Workstation">
+            <Tool title="16” MacBook Pro, M1 Max, 32GB RAM (2021)">
+              I was using an Intel-based 16” MacBook Pro prior to this and the
+              difference is night and day. I’ve never heard the fans turn on a
+              single time, even under the incredibly heavy loads I put it
+              through.
+            </Tool>
+          </ToolsSection>
+          {/* <ToolsSection title="Development tools">
+          </ToolsSection>
+          <ToolsSection title="Productivity">
+          </ToolsSection> */}
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
