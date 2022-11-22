9d96ba351ee914f132bcf2358043329eb5e14e30
Jorge Castello <jorgio.castello@gmail.com>
2022-11-21 23:02:10 -0800

    working on personal site

---
+++ src/constants/social-links.js
@@ -0,0 +1,5 @@
+export const TWITTER_LINK = 'https://twitter.com/jorgiocastello'
+export const INSTAGRAM_LINK = 'https://instagram.com/jorgio'
+export const GITHUB_LINK = 'https://github.com/jorge-castello'
+export const LINKEDIN_LINK =
+  'https://www.linkedin.com/in/jorge-castello-779002b2/'
