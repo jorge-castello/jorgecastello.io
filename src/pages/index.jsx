58a205643a29513d67e9ff29c7e6fe5befdec259
Jorge Castello <jorgio.castello@gmail.com>
2022-11-22 23:23:25 -0800

    and -> &

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -270,7 +270,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software developer, founder, and former finance bro.
+            Software developer, founder, & former finance bro.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Jorge, a software developer and entrepreneur based in San Diego. During the day, I work at Amazon in a team that generates $30B+ annual free cash flow. In my free time, I build software products and write articles.
