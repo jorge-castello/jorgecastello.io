f2f17aee0d63a3e7b2047ef3eb474e5d1e1f3433
Jorge Castello <jorgio.castello@gmail.com>
2022-12-09 13:43:23 -0800

    add article page copy

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Jorge Castello</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="A collection of my thoughts on programming and other topics I find interesting."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design and other topics I find interesting"
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Diving deep on software and beyond"
+        intro="A collection of my thoughts on programming and other topics I find interesting."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
