05d7c96befb8797641a81c8e82af21c88a9475a4
Jorge Castello <jorgio.castello@gmail.com>
2022-12-11 14:44:17 -0800

    minor clean up of backend implementation

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,9 +1,5 @@
 import { HttpRequests, HttpStatus } from '@/constants/http'
-import {
-  AddSubscriberResult,
-  addSubscriber,
-  AlreadySubscribedError,
-} from '@/lib/revue'
+import * as revue from '@/lib/revue'
 import { InvalidInputError } from '@/models/error'
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -15,18 +11,24 @@ export default async function handler(
   if (req.method !== HttpRequests.POST) {
     return res
       .status(HttpStatus.METHOD_NOT_ALLOWED)
-      .send({ message: 'Only POST requests allowed' })
+      .json({ message: 'Only POST requests allowed' })
   }
 
   try {
     const { email } = parseAndValidateRequest(req)
-    const addSubscriberResult: AddSubscriberResult = await addSubscriber(email)
 
-    return res.status(HttpStatus.OK).json(addSubscriberResult)
+    const addSubscriberResult: revue.AddSubscriberResult =
+      await revue.addSubscriber(email)
+
+    return res.status(HttpStatus.OK).json({
+      id: addSubscriberResult.id,
+      email: addSubscriberResult.email,
+      success: true,
+    })
   } catch (ex) {
     if (
       ex instanceof InvalidInputError ||
-      ex instanceof AlreadySubscribedError
+      ex instanceof revue.AlreadySubscribedError
     ) {
       return res.status(HttpStatus.BAD_REQUEST).json({ error: ex.message })
     }
