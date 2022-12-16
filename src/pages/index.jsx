2d655fd2d8587db284b578df8062ad636f18b4c3
JC <60675751+jorge-castello@users.noreply.github.com>
2022-12-15 21:03:27 -0800

    remove boilerplate (#23)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -330,7 +330,7 @@ export default function Home({ articles, resumeUrl }) {
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
-            {[].map((article) => (
+            {articles.map((article) => (
               <Article key={article.slug} article={article} />
             ))}
           </div>
